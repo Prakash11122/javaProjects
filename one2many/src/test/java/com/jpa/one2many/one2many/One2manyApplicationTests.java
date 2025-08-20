c58470342e044097528883de90f72419d929359d
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2025-08-20 23:27:00 +0530

    one2many

---
+++ one2many/src/test/java/com/jpa/one2many/one2many/One2manyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.one2many.one2many;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class One2manyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
